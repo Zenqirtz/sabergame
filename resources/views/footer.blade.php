2e29528ab35616e6398d67a4ff66f423dfbb74f9
Zenqirtz <argad5033@gmail.com>
2026-09-16 11:27:08 +0700

    feat(footer): tambah jam operasional, copyright dinamis, dan link sosial media (WA, IG, FB)

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,12 +1,33 @@
 <footer class="footer">
-    <div class="container footer-container">
-        <div>
-            <i class="fas fa-map-marker-alt"></i>
-            <span>Jl. Terusan Sudimoro No.6 Blok A2, Jatimulyo, Kec Lowokwaru, Kota Malang, Jawa Timur 65141</span>
+    <div class="footer-container">
+        <div class="footer-info">
+            <div>
+                <i class="fas fa-map-marker-alt"></i>
+                <span>Jl. Terusan Sudimoro No.6 Blok A2, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141</span>
+            </div>
+            <div>
+                <i class="fas fa-phone"></i>
+                <span>+62 878-5616-1934</span>
+            </div>
+            <div>
+                <i class="fas fa-clock"></i>
+                <span>Senin – Minggu, 09.00 – 22.00 WIB</span>
+            </div>
         </div>
-        <div>
-            <i class="fas fa-phone"></i>
-            <span>+62 878-5616-1934</span>
+
+        <div class="footer-bottom">
+            <span>&copy; {{ date('Y') }} Saber Game Malang. All rights reserved.</span>
+            <div class="footer-social">
+                <a href="https://wa.me/6287856161934" target="_blank" rel="noopener" title="WhatsApp Saber Game">
+                    <i class="fab fa-whatsapp"></i>
+                </a>
+                <a href="#" title="Instagram Saber Game">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="#" title="Facebook Saber Game">
+                    <i class="fab fa-facebook"></i>
+                </a>
+            </div>
         </div>
     </div>
 </footer>
